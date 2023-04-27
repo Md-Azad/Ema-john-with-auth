71d85dc4db8a6faf0d4cd61d7a5bc1c151ffe19a
Md-Azad <sokalazad@gmail.com>
2023-04-27 21:08:22 +0200

    added login and sign up form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Login.css";
+import "./SignUp.css";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <div className="form-controller">
-      <h3 className="form-title">Login</h3>
+      <h3 className="form-title">Sign Up</h3>
       <div className="form-control">
         <label htmlFor="email">Email</label>
         <input type="email" name="email" placeholder="Your Email" required />
@@ -19,10 +19,19 @@ const Login = () => {
         />
       </div>
       <div className="form-control">
-        <button className="btn-submit">Login</button>
+        <label htmlFor="confirm-password">Confirm Password</label>
+        <input
+          type="password"
+          name="confirm-password"
+          placeholder="Your password"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <button className="btn-submit">Sign Up</button>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default SignUp;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVrihiZUqHTkpsQix06x65H25BCY55ADI",
+  authDomain: "ema-jhon-with-auth-b977d.firebaseapp.com",
+  projectId: "ema-jhon-with-auth-b977d",
+  storageBucket: "ema-jhon-with-auth-b977d.appspot.com",
+  messagingSenderId: "645027900293",
+  appId: "1:645027900293:web:f147f6ff3f0d9c649d32ec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
