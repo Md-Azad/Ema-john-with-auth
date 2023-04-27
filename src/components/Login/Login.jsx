bc50b0d7c72d3000bfd20b6aafc5c0072704b88d
Md-Azad <sokalazad@gmail.com>
2023-04-27 22:36:28 +0200

    added 2 properties of validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
   return (
     <div className="form-controller">
       <h3 className="form-title">Login</h3>
-      <div className="form-control">
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" placeholder="Your Email" required />
-      </div>
-      <div className="form-control">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
-      </div>
-      <div className="form-control">
-        <button className="btn-submit">Login</button>
-      </div>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Your Email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <button className="btn-submit">Login</button>
+        </div>
+      </form>
+      <p className="form-title">
+        <small>
+          New to Ema-John? <Link to="/signup">Create New Account</Link>{" "}
+        </small>
+      </p>
     </div>
   );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,35 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
+  const [error, setError] = useState();
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirmPassword = form.confirmPassword.value;
+    console.log(email, password, confirmPassword);
+    if (password !== confirmPassword) {
+      setError("Password does not matched!!!");
+    } else if (password.length < 7) {
+      setError("Your Password smaller than 6 digits");
+    }
+  };
   return (
     <div className="form-controller">
       <h3 className="form-title">Sign Up</h3>
-      <div className="form-control">
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" placeholder="Your Email" required />
-      </div>
-      <div className="form-control">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
-      </div>
-      <div className="form-control">
-        <label htmlFor="confirm-password">Confirm Password</label>
-        <input
-          type="password"
-          name="confirm-password"
-          placeholder="Your password"
-          required
-        />
-      </div>
-      <div className="form-control">
-        <button className="btn-submit">Sign Up</button>
-      </div>
+      <form onSubmit={handleSignUp}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Your Email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Your password"
+            required
+          />
+        </div>
+        <p>{error}</p>
+        <div className="form-control">
+          <button className="btn-submit">Sign Up</button>
+        </div>
+      </form>
+      <p className="form-title">
+        <small>
+          Already Have An Account? <Link to="/login">Login</Link>{" "}
+        </small>
+      </p>
     </div>
   );
 };
